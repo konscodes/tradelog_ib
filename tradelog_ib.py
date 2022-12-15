8ff0a2845c733049f546f5673fc59a6e9729a5e2
Kons <katashi.rain@gmail.com>
2022-12-15 17:45:56 +0900

    Starting with a test connection to API

--- tradelog_ib.py
+++ tradelog_ib.py
@@ -1,1 +1,13 @@
-#test
+# Import EClient for outgoing requests and EWrapper for incoming messages
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+
+# Initiate the class for connection 
+class IBapi(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self, self)
+
+# Test the connection
+app = IBapi()
+app.connect('127.0.0.1', 7497, 123)
+app.run()

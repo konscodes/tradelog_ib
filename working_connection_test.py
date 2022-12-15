891a1bccfd2da9da6aefe1b7597fca0fb613c1f5
Kons <katashi.rain@gmail.com>
2022-12-15 19:37:10 +0900

    Connection test complete

--- working_connection_test.py
+++ working_connection_test.py
@@ -1,6 +1,7 @@
 # Import EClient for outgoing requests and EWrapper for incoming messages
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
+import time
 
 # Initiate the class for connection 
 class IBapi(EWrapper, EClient):
@@ -10,4 +11,10 @@ class IBapi(EWrapper, EClient):
 # Test the connection
 app = IBapi()
 app.connect('127.0.0.1', 7497, 123)
+
+# Run the script
 app.run()
+
+# End the session when the app is done
+time.sleep(2)
+app.disconnect()
